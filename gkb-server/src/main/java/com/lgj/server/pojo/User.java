fed8c5072af9818cceb3c2f9f6b6971f90d06d96
luoguangjin <2496487653@qq.com>
2022-03-02 11:49:00 +0800

    修改异常类

--- gkb-server/src/main/java/com/lgj/server/pojo/User.java
+++ gkb-server/src/main/java/com/lgj/server/pojo/User.java
@@ -1,5 +1,6 @@
 package com.lgj.server.pojo;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -35,29 +36,37 @@ public class User implements Serializable {
     private Integer id;
 
     @ApiModelProperty(value = "账号")
+    @Excel(name = "用户账号")
     private String username;
 
     @ApiModelProperty(value = "密码")
+    @Excel(name = "密码")
     private String password;
 
     @ApiModelProperty(value = "昵称")
+    @Excel(name = "昵称")
     private String nickname;
 
     @ApiModelProperty(value = "手机号码")
+    @Excel(name = "手机号码")
     private String phone;
 
     @ApiModelProperty(value = "性别")
+    @Excel(name = "性别")
     private String gender;
 
     @ApiModelProperty(value = "邮箱")
+    @Excel(name = "邮箱")
     private String email;
 
     @ApiModelProperty(value = "创建时间")
     @JsonFormat(pattern = "yyyy-MM-dd",timezone = "Asia/Shanghai")
+    @Excel(name = "创建时间", width=20, format = "yyyy-MM-dd")
     private LocalDateTime createTime;
 
     @JsonFormat(pattern = "yyyy-MM-dd",timezone = "Asia/Shanghai")
     @ApiModelProperty(value = "修改时间")
+    @Excel(name = "修改时间" ,width=20, format = "yyyy-MM-dd")
     private LocalDateTime updateTime;
 
 
